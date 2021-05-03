7b9a2eb4678d17e65ec2d1cf50fe740a1ef67dfc
daniellaga84@gmail.com <Erkano84!!>
2021-05-03 10:17:00 +0200

    Initial commit

---
+++ src/main/java/javer/drinkappspringversion/DrinkAppSpringVersionApplication.java
@@ -0,0 +1,13 @@
+package javer.drinkappspringversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkAppSpringVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkAppSpringVersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/javer/drinkappspringversion/DrinkAppSpringVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package javer.drinkappspringversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkAppSpringVersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
