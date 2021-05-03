da630ca4333ede7e4d7571ccb24ce989d071db9d
daniellaga84@gmail.com <Erkano84!!>
2021-05-03 13:46:06 +0200

    Added someconfiguration.

---
+++ src/main/java/javer/drinkappspringversion/controller/TemplateController.java
@@ -0,0 +1,28 @@
+package javer.drinkappspringversion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TemplateController {
+    @GetMapping("/")
+    public String start() {
+        return "age-query";
+    }
+
+    @GetMapping("/adult_check")
+    public String adultCheck(@RequestParam("age") String age) {
+        if (age.equals("18+")) {
+            return "GOOD";
+        } else {
+            return "redirect:goodbye";
+        }
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbye() {
+        return "goodbye";
+    }
+}
+
