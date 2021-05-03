5b95bee6b50fbf030a4231669ee99e21f4ff4b84
daniellaga84@gmail.com <Erkano84!!>
2021-05-03 10:26:40 +0200

    Made User in model class

---
+++ src/main/java/javer/drinkappspringversion/model/User.java
@@ -0,0 +1,31 @@
+package javer.drinkappspringversion.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
