3cadab60ffbea57adc3a3a004260e03916a7b2a2
daniellaga84@gmail.com <Erkano84!!>
2021-05-04 15:43:03 +0200

    Added user-view controller.

---
+++ src/main/java/javer/drinkappspringversion/service/web/controller/UserViewController.java
@@ -0,0 +1,29 @@
+package javer.drinkappspringversion.service.web.controller;
+
+import javer.drinkappspringversion.service.DrinkService;
+import javer.drinkappspringversion.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class UserViewController {
+
+    private final DrinkService drinkService;
+    private final UserService userService;
+
+    @GetMapping("/user-view")
+    public String showUserView(@RequestParam Integer page, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("drinks",drinkService.getAllDrinks());
+        model.addAttribute("pageNumbers", drinkService.countsPages(12));
+        model.addAttribute("requestDrinks", drinkService.getRequestDrinkList(page,12));
+        model.addAttribute("user",userService.get(authentication.getName()));
+        return "user-view";
+    }
+}
